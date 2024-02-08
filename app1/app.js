239f9b6f7f9935d40ac876cf471bc869ff9116e6
Luiz Gabriel <83136572+Luiz-Gabriel-ForFreelas@users.noreply.github.com>
2024-02-08 13:13:21 -0300

    Esboço da função para mostrar os itens do localStorage

--- app1/app.js
+++ app1/app.js
@@ -7,6 +7,15 @@ class Despesa {
         this.descricao = descricao
         this.valor = valor
     }
+
+    validarDados() {
+        for(let i in this) {
+            if(this[i] == undefined || this[i] == '' || this[i] == null) {
+                return false
+            }
+        }
+        return true
+    }
 }
 
 class Bd {
@@ -28,6 +37,22 @@ class Bd {
         localStorage.setItem(id, JSON.stringify(despesa))
         localStorage.setItem('id', id)
     }
+
+    recuperarTodosRegistros() {
+        let despesas = Array()
+        let id = localStorage.getItem('id')
+        for(let i = 1; i <= id; i++) {
+            let despesa = JSON.parse(localStorage.getItem(i))
+
+            if(despesa === null) {
+                continue
+            }
+
+            despesas.push(despesa)
+        }
+
+        console.log(despesas)
+    }
 }
 
 let bd = new Bd
@@ -42,5 +67,24 @@ function cadastrarDespesa() {
 
     let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
 
-    bd.gravarDespesa(despesa)
+    if (despesa.validarDados()) {
+        bd.gravarDespesa(despesa)
+        ajustarModal(true)
+        $('#modalRegistroDespesa').modal('show')
+    } else {
+        ajustarModal(false)
+        $('#modalRegistroDespesa').modal('show')
+    }
+}
+
+function ajustarModal(sucesso) {
+    document.getElementById('modal_titulo').innerHTML = (sucesso===true?'Registro realizado com sucesso':'Erro ao registrar')
+    document.getElementById('modal_header').className = (sucesso===true?'modal-header text-success':'modal-header text-danger')
+    document.getElementById('modal_body').innerHTML = (sucesso===true?'Despeza cadastrada com sucesso.':'Preencha todos os campos *')
+    document.getElementById('modal_btn').innerHTML = (sucesso===true?'Voltar':'Cancelar')
+    document.getElementById('modal_btn').className = (sucesso===true?'btn btn-success':'btn btn-danger')
+}
+
+function carregaListaDespesas() {
+    bd.recuperarTodosRegistros()
 }
